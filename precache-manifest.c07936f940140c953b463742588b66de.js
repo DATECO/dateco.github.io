97af8b326c8245d4a04d843ea15d27b390b9f9af
wborgeaud <williamborgeaud@gmail.com>
2020-06-27 17:16:34 +0200

    first

---
+++ precache-manifest.c07936f940140c953b463742588b66de.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "91abec4f267fe39eee2e",
+    "url": "/static/css/main.32477b77.chunk.css"
+  },
+  {
+    "revision": "91abec4f267fe39eee2e",
+    "url": "/static/js/main.91abec4f.chunk.js"
+  },
+  {
+    "revision": "1e2bb9f19726e6944140",
+    "url": "/static/js/1.1e2bb9f1.chunk.js"
+  },
+  {
+    "revision": "6a1280190f2426b8892f",
+    "url": "/static/js/2.6a128019.chunk.js"
+  },
+  {
+    "revision": "f413377fcaa6a11b9ddd",
+    "url": "/static/css/3.6c39f7d0.chunk.css"
+  },
+  {
+    "revision": "f413377fcaa6a11b9ddd",
+    "url": "/static/js/3.f413377f.chunk.js"
+  },
+  {
+    "revision": "9c72a6cf5098f93e5484",
+    "url": "/static/js/4.9c72a6cf.chunk.js"
+  },
+  {
+    "revision": "d4e352039489e5ef6e40",
+    "url": "/static/js/runtime~main.d4e35203.js"
+  },
+  {
+    "revision": "414cae0ed2e4980f9672190203222053",
+    "url": "/static/media/logo.414cae0e.svg"
+  },
+  {
+    "revision": "d27ae778088baaa3993ee76549ecd5ec",
+    "url": "/static/media/mountains.d27ae778.svg"
+  },
+  {
+    "revision": "5cd7e8c6e0be4c134dbbe712eff56907",
+    "url": "/static/media/windmills.5cd7e8c6.svg"
+  },
+  {
+    "revision": "377566ab98388ebbb82dd5b39e6f777f",
+    "url": "/static/media/iceberg.377566ab.svg"
+  },
+  {
+    "revision": "fe47e582d818373f5c75de3e404a7012",
+    "url": "/static/media/forestMin.fe47e582.jpg"
+  },
+  {
+    "revision": "0255a2be248df04c45b86641164afead",
+    "url": "/static/media/lausanne4.0255a2be.jpg"
+  },
+  {
+    "revision": "2508df2055ae3e150ddc505e107f95e7",
+    "url": "/static/media/information.2508df20.svg"
+  },
+  {
+    "revision": "09f20cc4e804368af1266eff0c0f4233",
+    "url": "/index.html"
+  }
+];
